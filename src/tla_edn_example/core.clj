80c722d7381d5e2ee1e6d4aa1830a59a23f710ab
Rafael Feodrippe <pfeodrippe@gmail.com>
2020-05-01 17:03:07 -0300

    add comments to the clojure code

--- src/tla_edn_example/core.clj
+++ src/tla_edn_example/core.clj
@@ -5,7 +5,8 @@
   (:import
    (java.io File)))
 
-;; see `resources/example.tla`
+;; see `resources/example.tla`, this is the position
+;; of the variables in the spec
 (def vars-keys
   [:c1 :c2 :account :sender :receiver :money :pc])
 
@@ -13,12 +14,15 @@
 ;; can be used to override a operator (check its args to see the options)
 (spec/defop TransferMoney {:module "example"}
   "`vars` is used to get the state of the world (:money, :sender, :receiver)"
-  [self account vars]
-  (let [self (tla-edn/to-edn self)
+  [self account vars]                   ; the operators arguments (check `resources/example.tla`)
+  (let [self (tla-edn/to-edn self) ; `self` is a string indicating which process is calling this operator
         vars (zipmap vars-keys (tla-edn/to-edn vars))
         sender (get-in vars [:sender self])
         receiver (get-in vars [:receiver self])
         money (get-in vars [:money self])]
+    ;; here we convert the record `account` and
+    ;; transfer the money from `sender` to `receiver`
+    ;; and then we convert it back to TLA values
     (-> (tla-edn/to-edn account)
         (update sender - money)
         (update receiver + money)
@@ -27,6 +31,10 @@
 ;; entry point
 (defn -main
   []
+  ;; run the spec
+  ;; first arg is the path to the TLA spec
+  ;; second arg is the config
   (spec/run-spec (.getAbsolutePath (File. "resources/example.tla"))
                  "example.cfg")
+  ;; exit the program with status 0
   (System/exit 0))
